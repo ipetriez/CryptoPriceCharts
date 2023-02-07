a8d4991d849542e42845db3223001d375c494007
Sergey Petrosyan <ipetriez@icloud.com>
2023-02-07 23:08:24 +0400

    Initial Commit

---
+++ CryptoPriceCharts/CryptoPriceChartsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoPriceChartsApp.swift
+//  CryptoPriceCharts
+//
+//  Created by Sergey Petrosyan on 07.02.23.
+//
+
+import SwiftUI
+
+@main
+struct CryptoPriceChartsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
